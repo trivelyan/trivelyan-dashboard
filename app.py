cbd0e136b0c00efff5279fb94a693e20e4cd0fcc
Talha Çelik <tlhcelik81@gmail.com>
2019-04-14 16:00:48 +0300

    get old listen port

--- app.py
+++ app.py
@@ -253,4 +253,4 @@ def login_test():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
-    app.run(debug=True, host='0.0.0.0', port=40)
+    app.run(debug=True, host='0.0.0.0', port=5000)
